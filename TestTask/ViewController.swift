7e816f488090935a2ae7047a8e266273018c00fe
Oksana Kovalchuk <oksana@anoda.mobi>
2018-07-03 10:46:01 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Oksana Kovalchuk on 7/3/18.
+//  Copyright © 2018 Oksana Kovalchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
